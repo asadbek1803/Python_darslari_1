6855f566426c115def7bfa849e7b2205f3a3a517
Oyatillo Abdullayev <132444569+Oyatillo2006@users.noreply.github.com>
2023-08-28 04:50:06 -0800

    Update 18-dars-amaliyot-javobi.py
    Chayildi

--- 18-dars-amaliyot-javobi.py
+++ 18-dars-amaliyot-javobi.py
@@ -1,29 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jan 18 17:30:21 2023
 
-@author: asadbek
-"""
-# mahsulotlar = []
-# ishora = True
-# while ishora:
-#     mahsulotlar.append(input("Mahsulot nomini kiriting\n>>"))
-#     javob = str(input("Davom etishdi istaysizmi (Ha/Yo'q)"))
-#     if javob == 'yo\'q':
-#         ishora = False
-#         break
-# print("Dastur tugadi!")    
-    
-
-e_bozor_mah = ['']
-mahsulotlar = []
-e_bozor_1 = {'go\'sht': 3000, 'kartoshka ':6000 ,' piyoz':5000 , 'sabzi':3000 }
-buyurtmalar = ['olma','piyoz','kartoshka','sabzi']
-ishora = True
-# while ishora:
-    
-#     mahsulotlar.append(input("Mahsulot nomini kiriting\n>>"))
-   # mahsulot = mahsulotlar.pop()
 #     narx = int(input("Narxini kiriting\n>>"))
     #
 #     javob = str(input("Davom etishdi istaysizmi (Ha/Yo'q)"))
